f0c06ff0ecca30dcf9ddd434e34b5ab0efac9c39
AssADev <gravelteo.pro@gmail.com>
2021-10-24 20:10:43 +0200

    Update Style & Add JS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 let express = require("express");
 let app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
+    res.render("login.ejs");
+});
+
+app.get("/home", function (req, res) {
     res.render("home.ejs");
 });
 
