b521fc41738c66c6981fbd082b81a56f04f8c84d
AssADev <gravelteo.pro@gmail.com>
2021-10-26 19:44:38 +0200

    Home Responsive Added

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,4 +1,17 @@
-const roomCards = document.querySelectorAll('.roomlist article.roomcard');
-const roomCardsDraggableBtns = document.querySelectorAll('.roomlist article.roomcard .head button#draggable');
+const roomCards = document.querySelectorAll(".roomlist article.roomcard");
+const roomCardsDraggableBtns = document.querySelectorAll(".roomlist article.roomcard .head button#draggable");
+const roomCardsToggleBtns = document.querySelectorAll(".roomlist article.roomcard .head button#onoff");
 
-console.log(roomCards, roomCardsDraggableBtns);
+// Hide Draggable buttons :
+if (roomCards.length <= 1) {
+    roomCardsDraggableBtns.forEach((roomCardsDraggableBtn) => {
+        roomCardsDraggableBtn.style.display = "none";
+    });
+}
+
+// Change Toggle On / Off buttons :
+roomCardsToggleBtns.forEach((roomCardsToggleBtn) =>
+    roomCardsToggleBtn.addEventListener("click", () => {
+        roomCardsToggleBtn.classList.toggle("down");
+    })
+);
