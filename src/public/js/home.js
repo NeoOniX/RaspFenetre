b351e4e4bae85b84c0bb711dca215bd8947e0162
AssA <gravelteo.pro@gmail.com>
2021-10-26 17:01:30 +0200

    home.js Started

---
+++ src/public/js/home.js
@@ -0,0 +1,4 @@
+const roomCards = document.querySelectorAll('.roomlist article.roomcard');
+const roomCardsDraggableBtns = document.querySelectorAll('.roomlist article.roomcard .head button#draggable');
+
+console.log(roomCards, roomCardsDraggableBtns);
