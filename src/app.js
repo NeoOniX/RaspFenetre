b46ab18d8941654cb70241ea9d0e6336487b6479
AssA <gravelteo.pro@gmail.com>
2021-10-26 15:15:51 +0200

    Added Header.ejs

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-let express = require("express");
+let express = require('express');
 let app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", function (req, res) {
-    res.render("login.ejs");
+app.get('/', function (req, res) {
+    res.render('login.ejs');
 });
 
-app.get("/home", function (req, res) {
-    res.render("home.ejs");
+app.get('/home', function (req, res) {
+    res.render('home.ejs');
 });
 
 app.use(function (req, res, next) {
-    res.setHeader("Content-Type", "text/plain");
-    res.status(404).send("Page introuvable !");
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
 });
 
 app.listen(80);
